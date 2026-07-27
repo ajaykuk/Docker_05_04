14129dc8f3b60356aeccac6148eb7ea04e4a8858
Ajay <30627649+ajaykuk@users.noreply.github.com>
2026-07-27 19:38:57 +0530

    updating

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import {MongoClient} from 'mongodb';
 
 const app = express();
 const PORT = 4000;
-const url = 'mongodb://localhost:27017';
+//const url = 'mongodb://localhost:27017';
+const url = 'mongodb://host.docker.internal:27017';
 const client = new MongoClient(url);
 
 // Database Name
@@ -11,14 +12,17 @@ const dbName = 'testDB';
 
 async function main() {
   await client.connect();
+  console.log('Connected successfully to MongoDB');
   const db = client.db(dbName);
   const collection = db.collection('documents');
 
   return 'done.';
 }
 
+main().catch(console.error);
+
 app.get('/', (req, res) => {
-    res.json("I love this docker container");
+    res.json("I love this docker new container");
 });
 
 app.listen(PORT, () => {
